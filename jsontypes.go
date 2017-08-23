9f2a31087ef576ff242a36e96c27fd01fde750c8
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2017-08-23 14:54:12 +0100

    String type that can handle a json string value or an array of strings

---
+++ jsontypes.go
@@ -0,0 +1,46 @@
+package jsontypes
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+)
+
+var (
+	ErrInvalidValue = errors.New("invalid JSON value")
+)
+
+type Strings []string
+
+func (a Strings) Contains(s string) bool {
+	for _, v := range a {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *Strings) UnmarshalJSON(b []byte) error {
+	d := json.NewDecoder(bytes.NewBuffer(b))
+	t, err := d.Token()
+	if err != nil {
+		return err
+	}
+	v := []string(*a)
+	if t == json.Delim('[') {
+		var s []string
+		if err := json.Unmarshal(b, &s); err != nil {
+			return err
+		}
+		v = append(v, s...)
+	} else if s, ok := t.(string); ok {
+		v = append(v, s)
+	} else if t == nil {
+		v = nil
+	} else if !ok {
+		return ErrInvalidValue
+	}
+	*a = Strings(v)
+	return nil
+}
